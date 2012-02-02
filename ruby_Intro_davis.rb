98e61970c7db8e368844189c544f944a38d4c941
Davis Godbout <dmgodbout@gmail.com>
2012-02-02 14:32:21 -0700

    class changes

--- ruby_Intro_davis.rb
+++ ruby_Intro_davis.rb
@@ -1,23 +1,19 @@
 class RubyIntro
+   
+  def self.date
+    Time.new
+  end 
 
+  def self.numbers
+    (1..1000).each {|x| puts x}
+  end
 
-   def RubyIntro.date
-      Time.new
-   end 
+  def self.add_numbers(a,b)
+    a + b
+  end
+
+  def self.stringcheck(help, word)
+    full_string.include? help
+  end
+
 end
-
-def RubyIntro.davis
-(1..1000).each {|x| puts x}
-end
-
-def RubyIntro.addnumbers(a,b)
-  a + b;
-end
-
-
-
-def RubyIntro.stringcheck(string,word)
-  full_string.include? check_string
-end
-
-
