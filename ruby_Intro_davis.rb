27435e9ff8a3b6384d659caa7d2b09fa1e8690f5
Davis Godbout <dmgodbout@gmail.com>
2012-02-02 12:59:13 -0700

    first hw

---
+++ ruby_Intro_davis.rb
@@ -0,0 +1,23 @@
+class RubyIntro
+
+
+   def RubyIntro.date
+      Time.new
+   end 
+end
+
+def RubyIntro.davis
+(1..1000).each {|x| puts x}
+end
+
+def RubyIntro.addnumbers(a,b)
+  a + b;
+end
+
+
+
+def Ruby Intro.stringcheck(string,word)
+  string.include? word_wrap
+end
+
+
